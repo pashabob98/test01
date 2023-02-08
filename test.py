f599d4f0869eb372be15558ce85502aa16e16318
DPU_03_015 <DPU_03_015@stud.kai.ru>
2023-02-08 10:49:58 +0300

    dos

---
+++ test.py
@@ -0,0 +1,12 @@
+class Chel:
+	def __init__(self, name, age):
+		self.name = name
+		self.age = age
+
+	def func1():
+		print("hello "+name+" my old friend" )
+
+if __name__ == "__main__":
+	
+	ch1 = Chel("Damir", "18")
+	ch1.func1()
